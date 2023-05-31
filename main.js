874e1a902c3fb875ce30a493a570787ded027391
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-31 11:44:14 -0700

    added murphy pics

--- main.js
+++ main.js
@@ -1,32 +1,39 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
+let scene, camera, renderer, cube;
 
-//perspective projection camera
-//takes in fov, aspect, near, far
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+function init() {
+  scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
+  //perspective projection camera
+  //takes in fov, aspect, near, far
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-renderer.setSize(window.innerWidth, window.innerHeight);
+  //antialias makes the edges look sharper
+  renderer = new THREE.WebGLRenderer({ antialias: true });
 
-document.body.appendChild(renderer.domElement);
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-//creates cube. parameters are width, height, and depth
-const geometry = new THREE.BoxGeometry(2, 2, 2);
+  document.body.appendChild(renderer.domElement);
 
-//needs an 8 digit hex code. Why? Last 2 digits are alpha transparency
-const material = new THREE.MeshBasicMaterial({ color: 949494e8 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  //creates cube. parameters are width, height, and depth
+  const geometry = new THREE.BoxGeometry(2, 2, 2);
 
-//by default, the coordinates will put the camera and cube inside of each other. We need to reposition the camera.
-camera.position.z = 5;
+  //needs an 8 digit hex code. Why? Last 2 digits are alpha transparency
+//   const material = new THREE.MeshBasicMaterial({ color: 949494e8 });
+  const texture = new THREE.TextureLoader().load('/textures/murphy.jpg')
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  //by default, the coordinates will put the camera and cube inside of each other. We need to reposition the camera.
+  camera.position.z = 5;
+}
 
 //redraws scene. Typical monitor is 60 frames per second.
 function animate() {
@@ -39,4 +46,17 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+//this will keep the cube in the middle when we resize the screen
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+//false means the event will be handled during the bubbling phase
+window.addEventListener("resize", onWindowResize, false);
+
+init();
 animate();
+
+
