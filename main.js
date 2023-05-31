41f9a071335c52d8e0dc1c9d835e23719e900afa
Kasey Knudsen <kaseyknudsen@gmail.com>
2023-05-31 09:49:52 -0700

    added code to main.js

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene()
+
+//perspective projection camera
+//takes in fov, aspect, near, far
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
